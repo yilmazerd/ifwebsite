41f664ef9ca5e3b43e0dfa168c695d00894136a1
yilmazerd <yilmazerdem@gmail.com>
2021-03-06 23:19:01 -0500

    minor, change edison21 to edison and 502 to 200

--- loadExamples.js
+++ loadExamples.js
@@ -3,13 +3,13 @@
 exampleOneCode+= 'package examples;\n'
 exampleOneCode+= '/*\n'
 exampleOneCode+= 'Example\n' 
-exampleOneCode+= 'Returns a very simple JSON structure and 502 HTTP code\n'
+exampleOneCode+= 'Returns a very simple JSON structure and 200 HTTP code\n'
 exampleOneCode+= "Follow this example. Do not log or stdout, if you'd like to use the custom http code and json.\n"
 exampleOneCode+= '*/\n'
 exampleOneCode+= 'public class DemoClass{\n'
 exampleOneCode+= 'public static void main(String[] args) {\n'
-exampleOneCode+= 'Person person = new Person("Thomas21","Edison");\n'
-exampleOneCode+= 'InstantFunctionResponse.responseBuilder(502,person.toString());\n'
+exampleOneCode+= 'Person person = new Person("Thomas","Edison");\n'
+exampleOneCode+= 'InstantFunctionResponse.responseBuilder(200,person.toString());\n'
 exampleOneCode+= '}\n'
 exampleOneCode+= '}\n'
 
@@ -35,7 +35,7 @@ exampleOneCode+= '            System.out.println("responseObject:"+responseObjec
 exampleOneCode+= '        }\n'
 
 exampleOneCode+= '        public static void responseBuilder(String responseObject) {\n'
-exampleOneCode+= '            responseBuilder(502, responseObject);\n'
+exampleOneCode+= '            responseBuilder(200, responseObject);\n'
 exampleOneCode+= '        }\n'
 
 exampleOneCode+= '    }\n'
